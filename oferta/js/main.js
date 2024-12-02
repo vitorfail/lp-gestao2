9209f96dd533d81833b9de348a4087c863ac7bfe
Vitor Manoel de andrade <95591747+vitorfail@users.noreply.github.com>
2024-12-02 08:28:19 -0300

    Update main.js

--- oferta/js/main.js
+++ oferta/js/main.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const dataAtual = new Date();
 
 // Data específica (28 de novembro de 2024)
-  const dataFutura = new Date(2024, 11, 2); 
+  const dataFutura = new Date(2024, 11, 3); 
 
 // Convertendo a diferença para segundos
   // Set up FlipDown
