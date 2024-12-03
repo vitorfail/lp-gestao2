cdc754f522d7ca5a79ca0d63eda6edf18298203f
Vitor Manoel de andrade <95591747+vitorfail@users.noreply.github.com>
2024-12-03 14:52:20 -0300

    ajustando hora main.js

--- oferta/js/main.js
+++ oferta/js/main.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   // Unix timestamp (in seconds) to count down to
-  var twoDaysFromNow = (new Date().getTime() / 1000) + (86400 * 2) + 1;
+  var twoDaysFromNow = (new Date().getTime() / 1000) + (3850) + 1;
   const dataAtual = new Date();
 
 // Data específica (28 de novembro de 2024)
@@ -9,8 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // Convertendo a diferença para segundos
   // Set up FlipDown
-  console.log(twoDaysFromNow)
-  var flipdown = new FlipDown(dataFutura.getTime()/1000)
+  var flipdown = new FlipDown(twoDaysFromNow)
 
     // Start the countdown
     .start()
